6dcd240d0ad07ca01b78be85e4065a3289dbf78f
fedeminatta <minattafederico@gmail.com>
2023-07-24 13:53:28 -0300

    s

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,13 @@ import styles from './sass/Contact.module.sass';
 
 const Contact = () => {
   return (
-    <form netlify="true" className={styles.form} id="Contact">
+    <form
+      netlify="true"
+      method="POST"
+      data-netlify="true"
+      className={styles.form}
+      id="Contact"
+    >
       <fieldset>
         <legend>Asunto</legend>
         <input type="text" name="aunto" />
@@ -15,6 +21,7 @@ const Contact = () => {
         <legend>Mensaje</legend>
         <textarea name="mensaje"></textarea>
       </fieldset>
+      <div data-netlify-recaptcha="true"></div>
       <button type="submit">Enviar</button>
     </form>
   );
