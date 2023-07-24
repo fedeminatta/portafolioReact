54f47703b70998dc3f179ffa1ab690ea1d0e17d8
fedeminatta <minattafederico@gmail.com>
2023-07-24 14:15:42 -0300

    s

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,15 +13,15 @@ const Contact = () => {
       <input type="hidden" name="form-name" value="contact" />
       <fieldset>
         <legend>Asunto</legend>
-        <input type="text" name="asunto" />
+        <input required type="text" name="asunto" />
       </fieldset>
       <fieldset>
         <legend>Correo</legend>
-        <input type="email" name="email" />
+        <input required type="email" name="email" />
       </fieldset>
       <fieldset className={styles.textarea}>
         <legend>Mensaje</legend>
-        <textarea name="mensaje"></textarea>
+        <textarea required name="mensaje"></textarea>
       </fieldset>
       {/* <div data-netlify-recaptcha="true"></div> */}
       <button type="submit">Enviar</button>
