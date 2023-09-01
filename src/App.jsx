62a17580bebff66f9a3661a2b9668e4118d3b3f7
fedeminatta <minattafederico@gmail.com>
2023-09-01 17:27:00 -0300

    loader

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,59 @@ import Tools from './components/Tools';
 import Contact from './components/Contact';
 import Sidebar from './components/Sidebar';
 import AnimatedCursor from 'react-animated-cursor';
+import { useEffect, useState } from 'react';
+import styles from './components/sass/App.module.sass';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener('DOMContentLoaded', () => {
+      setIsLoading(false);
+    });
+
+    setTimeout(() => {
+      if (document.readyState === 'complete') {
+        setIsLoading(false);
+      }
+    }, 1000);
+  }, []);
+
   return (
-    <div className="App">
-      <AnimatedCursor
-        innerSize={20}
-        outerSize={65}
-        innerScale={1}
-        outerScale={1.8}
-        outerAlpha={0}
-        hasBlendMode={true}
-        innerStyle={{
-          background: '#34d7f7',
-          zIndex: '12111',
-          mixBlendMode: 'difference',
-        }}
-        outerStyle={{
-          border: 'solid 3px #34d7f7',
-          boxShadow: '0 0 15px #34d7f7',
-          zIndex: '12111',
-          mixBlendMode: 'difference',
-        }}
-      />
-      <Header />
-      <Sidebar />
-      <About />
-      <Bg />
-      <Tools />
-      <Contact />
-    </div>
+    <>
+      {isLoading ? (
+        <div className={styles.ldsHeart}>
+          <div></div>
+        </div>
+      ) : (
+        <div className="App">
+          <AnimatedCursor
+            innerSize={20}
+            outerSize={65}
+            innerScale={1}
+            outerScale={1.8}
+            outerAlpha={0}
+            hasBlendMode={true}
+            innerStyle={{
+              background: '#34d7f7',
+              zIndex: '12111',
+              mixBlendMode: 'difference',
+            }}
+            outerStyle={{
+              border: 'solid 3px #34d7f7',
+              boxShadow: '0 0 15px #34d7f7',
+              zIndex: '12111',
+              mixBlendMode: 'difference',
+            }}
+          />
+          <Header />
+          <Sidebar />
+          <About />
+          <Bg />
+          <Tools />
+          <Contact />
+        </div>
+      )}
+    </>
   );
 }
