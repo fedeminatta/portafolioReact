a80544d410fa2c857e48260f3bcb1fcc512a0f19
fedeminatta <minattafederico@gmail.com>
2023-09-01 16:44:03 -0300

    cursor animation and select color

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
 import Bg from './components/Bg';
 import Tools from './components/Tools';
 import Contact from './components/Contact';
 import Sidebar from './components/Sidebar';
+import AnimatedCursor from 'react-animated-cursor';
 
-function App() {
+export default function App() {
   return (
-    <>
+    <div className="App">
+      <AnimatedCursor
+        innerSize={20}
+        outerSize={65}
+        innerScale={1}
+        outerScale={1.8}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          background: '#34d7f7',
+          zIndex: '12111',
+        }}
+        outerStyle={{
+          border: 'solid 3px #34d7f7',
+          boxShadow: '0 0 15px #34d7f7',
+          mixBlendMode: 'difference',
+        }}
+      />
       <Header />
       <Sidebar />
       <About />
       <Bg />
       <Tools />
       <Contact />
-    </>
+    </div>
   );
 }
-
-export default App;
