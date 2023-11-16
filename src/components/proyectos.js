68f417fad7312c01e1aa6eb076b161d3f2eb281c
fedeminatta <minattafederico@gmail.com>
2023-11-16 10:51:19 -0300

    ecommerce

--- src/components/proyectos.js
+++ src/components/proyectos.js
@@ -12,160 +12,161 @@ import piedra from '../img/trabajos/piedra.webp';
 import restaurant from '../img/trabajos/restaurant.webp';
 import veterinariaJS from '../img/trabajos/veterinariaJS.webp';
 import veterinariaReact from '../img/trabajos/veterinariaReact.webp';
-import horarios from '../img/trabajos/horarios.webp'
-import postres from '../img/trabajos/postresTemplate.webp'
-// import flexbox from '../img/trabajos/flexbox.webp';
-// import cryptos from '../img/trabajos/cryptos.webp';
-// import gastoSemanal from '../img/trabajos/gasto-semanal.webp';
-// import sumaResta from '../img/trabajos/suma-resta.webp';
-// import hojas from '../img/trabajos/hojas.webp';
-// import monedas from '../img/trabajos/monedas.webp';
-// img
-
+import horarios from '../img/trabajos/horarios.webp';
+import postres from '../img/trabajos/postresTemplate.webp';
+import ecommerce from '../img/trabajos/ecommerce.png';
 const proyectos = [
-  {
-    id: nanoid(),
-    titulo: 'Horario Plan',
-    descripcion: 'HTML - Next JS - React - SASS - JavaScript',
-    img: horarios,
-    url: 'https://horarioplan.vercel.app/',
-  },
-  {
-    id: nanoid(),
-    titulo: 'Postres',
-    descripcion: 'HTML - Vite - React - SASS - JavaScript',
-    img: postres,
-    url: 'https://postres-template.vercel.app/',
-  },
-  {
-    id: nanoid(),
-    titulo: 'Juego de Memoria',
-    descripcion: 'HTML - SASS - JavaScript',
-    img: juegoMemoria,
-    url: 'https://juego-memoria-mf.netlify.app/',
-  },
-  {
-    id: nanoid(),
-    titulo: 'Notas React',
-    descripcion: 'HTML - SASS - React - JavaScript',
-    img: notasReact,
-    url: 'https://notas-mf.netlify.app/',
-  },
-  {
-    id: nanoid(),
-    titulo: 'ADM de Veterinaria React',
-    descripcion: 'HTML - Tailwind - CSS - React - JavaScript',
-    img: veterinariaReact,
-    url: 'https://citas-react-mf.netlify.app/',
-  },
-  {
-    id: nanoid(),
-    titulo: 'Gasto Semanal React',
-    descripcion: 'HTML - CSS - React - JavaScript',
-    img: reactGastos,
-    url: 'https://gastos-mf.netlify.app/',
-  },
-  {
-    id: nanoid(),
-    titulo: 'Carrito de Comidas',
-    descripcion: 'HTML - SASS - JavaScript',
-    img: carritoComidas,
-    url: 'https://carrito-comida-mf.netlify.app/',
-  },
+    {
+        id: nanoid(),
+        titulo: 'Ecommerce',
+        descripcion:
+            'HTML - Next JS - Strapi - React - Stripe - SASS - JavaScript',
+        img: ecommerce,
+        url: 'https://ecommerce-demo-federico-minatta.vercel.app/',
+    },
+    {
+        id: nanoid(),
+        titulo: 'Horario Plan',
+        descripcion: 'HTML - Next JS - React - SASS - JavaScript',
+        img: horarios,
+        url: 'https://horarioplan.vercel.app/',
+    },
+    {
+        id: nanoid(),
+        titulo: 'Postres',
+        descripcion: 'HTML - Vite - React - SASS - JavaScript',
+        img: postres,
+        url: 'https://postres-template.vercel.app/',
+    },
+    {
+        id: nanoid(),
+        titulo: 'Juego de Memoria',
+        descripcion: 'HTML - SASS - JavaScript',
+        img: juegoMemoria,
+        url: 'https://juego-memoria-mf.netlify.app/',
+    },
+    {
+        id: nanoid(),
+        titulo: 'Notas React',
+        descripcion: 'HTML - SASS - React - JavaScript',
+        img: notasReact,
+        url: 'https://notas-mf.netlify.app/',
+    },
+    {
+        id: nanoid(),
+        titulo: 'ADM de Veterinaria React',
+        descripcion: 'HTML - Tailwind - CSS - React - JavaScript',
+        img: veterinariaReact,
+        url: 'https://citas-react-mf.netlify.app/',
+    },
+    {
+        id: nanoid(),
+        titulo: 'Gasto Semanal React',
+        descripcion: 'HTML - CSS - React - JavaScript',
+        img: reactGastos,
+        url: 'https://gastos-mf.netlify.app/',
+    },
+    {
+        id: nanoid(),
+        titulo: 'Carrito de Comidas',
+        descripcion: 'HTML - SASS - JavaScript',
+        img: carritoComidas,
+        url: 'https://carrito-comida-mf.netlify.app/',
+    },
 
-  {
-    id: nanoid(),
-    titulo: 'Juego de Piedra Papel o Tijera',
-    descripcion: 'HTML - SASS - JavaScript',
-    img: piedra,
-    url: 'https://juego-piedra-mf.netlify.app/',
-  },
-  {
-    id: nanoid(),
-    titulo: 'CRM',
-    descripcion: 'HTML - CSS - JavaScript - Tailwind',
-    img: crmImg,
-    url: 'https://crm-mf.netlify.app/',
-  },
+    {
+        id: nanoid(),
+        titulo: 'Juego de Piedra Papel o Tijera',
+        descripcion: 'HTML - SASS - JavaScript',
+        img: piedra,
+        url: 'https://juego-piedra-mf.netlify.app/',
+    },
+    {
+        id: nanoid(),
+        titulo: 'CRM',
+        descripcion: 'HTML - CSS - JavaScript - Tailwind',
+        img: crmImg,
+        url: 'https://crm-mf.netlify.app/',
+    },
 
-  {
-    id: nanoid(),
-    titulo: 'Juego de Ahorcado',
-    descripcion: 'HTML - SASS - JavaScript',
-    img: ahorcado,
-    url: 'https://ahorcado-mf.netlify.app/',
-  },
-  // {
-  //   id: nanoid(),
-  //   titulo: 'Cotizador de Cryptos',
-  //   descripcion: 'HTML - CSS - React - JavaScript',
-  //   img: cryptos,
-  //   url: 'https://cryptos-mf.netlify.app/',
-  // },
-  {
-    id: nanoid(),
-    titulo: 'ADM de Veterinaria JS',
-    descripcion: 'HTML - SASS - JavaScript',
-    img: veterinariaJS,
-    url: 'https://veterinaria-mf.netlify.app/',
-  },
-  // {
-  //   id: nanoid(),
-  //   titulo: 'Gasto Semanal JS',
-  //   descripcion: 'HTML - CSS - Bootstrap - JavaScript',
-  //   img: gastoSemanal,
-  //   url: 'https://gasto-semanal-mf.netlify.app/',
-  // },
-  {
-    id: nanoid(),
-    titulo: 'Filtrador de Autos',
-    descripcion: 'HTML - SASS - JavaScript',
-    img: autos,
-    url: 'https://filtrador-autos.netlify.app/',
-  },
+    {
+        id: nanoid(),
+        titulo: 'Juego de Ahorcado',
+        descripcion: 'HTML - SASS - JavaScript',
+        img: ahorcado,
+        url: 'https://ahorcado-mf.netlify.app/',
+    },
+    // {
+    //   id: nanoid(),
+    //   titulo: 'Cotizador de Cryptos',
+    //   descripcion: 'HTML - CSS - React - JavaScript',
+    //   img: cryptos,
+    //   url: 'https://cryptos-mf.netlify.app/',
+    // },
+    {
+        id: nanoid(),
+        titulo: 'ADM de Veterinaria JS',
+        descripcion: 'HTML - SASS - JavaScript',
+        img: veterinariaJS,
+        url: 'https://veterinaria-mf.netlify.app/',
+    },
+    // {
+    //   id: nanoid(),
+    //   titulo: 'Gasto Semanal JS',
+    //   descripcion: 'HTML - CSS - Bootstrap - JavaScript',
+    //   img: gastoSemanal,
+    //   url: 'https://gasto-semanal-mf.netlify.app/',
+    // },
+    {
+        id: nanoid(),
+        titulo: 'Filtrador de Autos',
+        descripcion: 'HTML - SASS - JavaScript',
+        img: autos,
+        url: 'https://filtrador-autos.netlify.app/',
+    },
 
-  {
-    id: nanoid(),
-    titulo: 'Restaurante',
-    descripcion: 'HTML - SASS - JavaScript - jQuery',
-    img: restaurant,
-    url: 'https://restaurant-mf.netlify.app/',
-  },
-  {
-    id: nanoid(),
-    titulo: 'Formulario de Compras',
-    descripcion: 'HTML - CSS - JavaScript',
-    img: formCompras,
-    url: 'https://formulario-compras-mf.netlify.app/',
-  },
-  // {
-  //   id: nanoid(),
-  //   titulo: 'Apuntes de Flexbox CSS',
-  //   descripcion: 'HTML - SASS',
-  //   img: flexbox,
-  //   url: 'https://flexbox-mf.netlify.app/',
-  // },
-  // {
-  //   id: nanoid(),
-  //   titulo: 'Juego de Apuestas',
-  //   descripcion: 'HTML - SASS - JavaScript',
-  //   img: monedas,
-  //   url: 'https://monedas-mf.netlify.app/',
-  // },
-  // {
-  //   id: nanoid(),
-  //   titulo: 'Sumas y Restas',
-  //   descripcion: 'HTML - SASS - JavaScript',
-  //   img: sumaResta,
-  //   url: 'https://suma-resta-mf.netlify.app/',
-  // },
-  // {
-  //   id: nanoid(),
-  //   titulo: 'Página de Plantas',
-  //   descripcion: 'HTML - SASS',
-  //   img: hojas,
-  //   url: 'https://plantas-mf.netlify.app/',
-  // },
+    {
+        id: nanoid(),
+        titulo: 'Restaurante',
+        descripcion: 'HTML - SASS - JavaScript - jQuery',
+        img: restaurant,
+        url: 'https://restaurant-mf.netlify.app/',
+    },
+    {
+        id: nanoid(),
+        titulo: 'Formulario de Compras',
+        descripcion: 'HTML - CSS - JavaScript',
+        img: formCompras,
+        url: 'https://formulario-compras-mf.netlify.app/',
+    },
+    // {
+    //   id: nanoid(),
+    //   titulo: 'Apuntes de Flexbox CSS',
+    //   descripcion: 'HTML - SASS',
+    //   img: flexbox,
+    //   url: 'https://flexbox-mf.netlify.app/',
+    // },
+    // {
+    //   id: nanoid(),
+    //   titulo: 'Juego de Apuestas',
+    //   descripcion: 'HTML - SASS - JavaScript',
+    //   img: monedas,
+    //   url: 'https://monedas-mf.netlify.app/',
+    // },
+    // {
+    //   id: nanoid(),
+    //   titulo: 'Sumas y Restas',
+    //   descripcion: 'HTML - SASS - JavaScript',
+    //   img: sumaResta,
+    //   url: 'https://suma-resta-mf.netlify.app/',
+    // },
+    // {
+    //   id: nanoid(),
+    //   titulo: 'Página de Plantas',
+    //   descripcion: 'HTML - SASS',
+    //   img: hojas,
+    //   url: 'https://plantas-mf.netlify.app/',
+    // },
 ];
 
 export default proyectos;
