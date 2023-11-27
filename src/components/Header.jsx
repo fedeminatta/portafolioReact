a81d1af95d4f69922dd83f9167bcb412eb67bab8
fedeminatta <minattafederico@gmail.com>
2023-11-27 15:29:15 -0300

    img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,30 @@
 import styles from './sass/Header.module.sass';
-import personImg from '../img/person.svg';
-import { Fade, Slide, AttentionSeeker } from 'react-awesome-reveal';
+import personImg from '../img/person.png';
+import { Fade, Slide } from 'react-awesome-reveal';
 
 const Header = () => {
-  return (
-    <>
-      <nav className={styles.nav}>
-        <Fade cascade damping={0.3}>
-          <a href="#About">Sobre Mi</a>
-          <a href="#Main">Proyectos</a>
-          <a href="#Contact">Contacto</a>
-        </Fade>
-      </nav>
-      <header className={styles.header}>
-        <section className={styles.section}>
-          <Slide cascade damping={0.2}>
-            <h1>Federico Minatta</h1>
-            <h2>Frontend Developer</h2>
-          </Slide>
-        </section>
-        <AttentionSeeker effect="tada">
-          {' '}
-          <img src={personImg} alt="" />{' '}
-        </AttentionSeeker>
-      </header>
-    </>
-  );
+    return (
+        <>
+            <nav className={styles.nav}>
+                <Fade cascade damping={0.3}>
+                    <a href="#About">Sobre Mi</a>
+                    <a href="#Main">Proyectos</a>
+                    <a href="#Contact">Contacto</a>
+                </Fade>
+            </nav>
+            <header className={styles.header}>
+                <section className={styles.section}>
+                    <Slide cascade damping={0.2}>
+                        <h1>Federico Minatta</h1>
+                        <h2>Frontend Developer</h2>
+                    </Slide>
+                </section>
+                <Slide direction="right" triggerOnce>
+                    {' '}
+                    <img src={personImg} alt="" />{' '}
+                </Slide>
+            </header>
+        </>
+    );
 };
 export default Header;
