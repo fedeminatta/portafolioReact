bdc68f2af70aa1431194db7383740532864d4c70
fedeminatta <minattafederico@gmail.com>
2023-11-27 14:57:02 -0300

    form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,34 +2,31 @@ import { Slide } from 'react-awesome-reveal';
 import styles from './sass/Contact.module.sass';
 
 const Contact = () => {
-  return (
-    <Slide direction="right" triggerOnce>
-      <form
-        method="post"
-        data-netlify="true"
-        data-netlify-recaptcha="true"
-        className={styles.form}
-        action="/contact"
-        id="Contact"
-        name="contact"
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <fieldset>
-          <legend>Asunto</legend>
-          <input required type="text" name="asunto" />
-        </fieldset>
-        <fieldset>
-          <legend>Correo</legend>
-          <input required type="email" name="email" />
-        </fieldset>
-        <fieldset className={styles.textarea}>
-          <legend>Mensaje</legend>
-          <textarea required name="mensaje"></textarea>
-        </fieldset>
-        <div data-netlify-recaptcha="true"></div>
-        <button type="submit">Enviar</button>
-      </form>
-    </Slide>
-  );
+    return (
+        <Slide direction="right" triggerOnce>
+            <form
+                method="post"
+                data-netlify="true"
+                className={styles.form}
+                id="Contact"
+                name="contact"
+            >
+                <input name="form-name" value="contact" />
+                <fieldset>
+                    <legend>Asunto</legend>
+                    <input required type="text" name="asunto" />
+                </fieldset>
+                <fieldset>
+                    <legend>Correo</legend>
+                    <input required type="email" name="email" />
+                </fieldset>
+                <fieldset className={styles.textarea}>
+                    <legend>Mensaje</legend>
+                    <textarea required name="mensaje"></textarea>
+                </fieldset>
+                <button type="submit">Enviar</button>
+            </form>
+        </Slide>
+    );
 };
 export default Contact;
