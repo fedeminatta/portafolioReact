188df906a7b724c6fbded8db0aaf46d078c65627
fedeminatta <minattafederico@gmail.com>
2023-12-10 14:17:24 -0300

    arquitectura y astro

--- src/components/Iconos.jsx
+++ src/components/Iconos.jsx
@@ -11,83 +11,94 @@ import tailwindIcon from '@img/tecnologias/tailwind.png';
 import stripeIcon from '@img/tecnologias/stripe.svg';
 import strapiIcon from '@img/tecnologias/strapi.png';
 import jqueryIcon from '@img/tecnologias/jquery.svg';
+import astroIcon from '@img/tecnologias/astro.png';
 
 const Iconos = ({ title }) => {
-    let color;
-    let textColor;
-    let icon;
-    switch (title) {
-        case 'html':
-            color = '#f26323';
-            icon = htmlIcon;
-            textColor = '#fff';
-            break;
-        case 'css':
-            color = '#329bff';
-            icon = cssIcon;
-            textColor = '#fff';
-            break;
-        case 'next js':
-            color = '#d7d7d7';
-            icon = nextIcon;
-            break;
-        case 'react':
-            color = '#074677';
-            icon = reactIcon;
-            textColor = '#fff';
-            break;
-        case 'vite':
-            color = '#000';
-            icon = viteIcon;
-            textColor = '#fff';
-            break;
-        case 'jquery':
-            color = '#fff';
-            icon = jqueryIcon;
-            textColor = '#000';
-            break;
-        case 'sass':
-            color = '#d75395';
-            icon = sassIcon;
-            textColor = '#fff';
-            break;
-        case 'javascript':
-            color = '#000000';
-            icon = jsIcon;
-            textColor = '#fff';
-            break;
-        case 'tailwind':
-            color = '#fff';
-            icon = tailwindIcon;
-            textColor = '#000';
-            break;
-        case 'stripe':
-            color = '#fff';
-            icon = stripeIcon;
-            textColor = '#000';
-            break;
-        case 'strapi':
-            color = '#fff';
-            icon = strapiIcon;
-            textColor = '#000';
-            break;
-        default:
-            break;
-    }
-    return (
-        <>
-            <div
-                className={styles.iconos}
-                style={{
-                    backgroundColor: color,
-                    color: textColor,
-                    boxShadow: `0px 0px 8px ${color}`,
-                }}
-            >
-                <img className={styles.img} src={icon} alt={title} />
-                <p>{title}</p>
-            </div>
-        </>
-    );
+	let color;
+	let textColor;
+	let icon;
+	switch (title) {
+		case 'html':
+			color = '#f26323';
+			icon = htmlIcon;
+			textColor = '#fff';
+			break;
+		case 'css':
+			color = '#329bff';
+			icon = cssIcon;
+			textColor = '#fff';
+			break;
+		case 'next js':
+			color = '#d7d7d7';
+			icon = nextIcon;
+			break;
+		case 'react':
+			color = '#074677';
+			icon = reactIcon;
+			textColor = '#fff';
+			break;
+		case 'vite':
+			color = '#000';
+			icon = viteIcon;
+			textColor = '#fff';
+			break;
+		case 'jquery':
+			color = '#fff';
+			icon = jqueryIcon;
+			textColor = '#000';
+			break;
+		case 'sass':
+			color = '#d75395';
+			icon = sassIcon;
+			textColor = '#fff';
+			break;
+		case 'scss':
+			color = '#d75395';
+			icon = sassIcon;
+			textColor = '#fff';
+			break;
+		case 'javascript':
+			color = '#000000';
+			icon = jsIcon;
+			textColor = '#fff';
+			break;
+		case 'tailwind':
+			color = '#fff';
+			icon = tailwindIcon;
+			textColor = '#000';
+			break;
+		case 'stripe':
+			color = '#fff';
+			icon = stripeIcon;
+			textColor = '#000';
+			break;
+		case 'strapi':
+			color = '#fff';
+			icon = strapiIcon;
+			textColor = '#000';
+			break;
+		case 'astro':
+			color = '#fff';
+			icon = astroIcon;
+			textColor = '#000';
+			break;
+		default:
+			break;
+	}
+	return (
+		<>
+			<div
+				className={styles.iconos}
+				style={{
+					backgroundColor: color,
+					color: textColor,
+					boxShadow: `0px 0px 8px ${color}`,
+				}}
+			>
+				<img className={styles.img} src={icon} alt={title} />
+				<p>{title}</p>
+			</div>
+		</>
+	);
 };
 export default Iconos;
--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -11,158 +11,117 @@ import gitImg from '../img/tecnologias/git.png';
 import cssImg from '../img/tecnologias/css.png';
 import nextjsImg from '../img/tecnologias/nextjs.svg';
 import tsImg from '../img/tecnologias/ts.svg';
+import astroImg from '../img/tecnologias/astro.png';
 import { Zoom } from 'react-awesome-reveal';
 //
 
 const Tools = () => {
-    return (
-        <section className={styles.section} id="tools">
-            <h3>Tecnologías</h3>
-            <article className={styles.article}>
-                <Zoom cascade duration={200} triggerOnce>
-                    <div className={styles.item}>
-                        <a
-                            href="https://react.dev/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <img
-                                className={styles.react}
-                                src={reactImg}
-                                alt="React icon"
-                            />
-                        </a>
-                        <p>React</p>
-                    </div>
-                    <div className={styles.item}>
-                        <a
-                            href="https://vitejs.dev/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <img
-                                className={styles.vite}
-                                src={viteImg}
-                                alt="Vite icon"
-                            />
-                        </a>
-                        <p>Vite</p>
-                    </div>
-                    <div className={styles.item}>
-                        <a
-                            href="https://nextjs.org/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <img
-                                className={styles.nextjs}
-                                src={nextjsImg}
-                                alt="Next JS icon"
-                            />
-                        </a>
-                        <p>Next JS</p>
-                    </div>
-                    <div className={styles.item}>
-                        <a
-                            href="https://developer.mozilla.org/es/docs/Web/JavaScript"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <img
-                                className={styles.javascript}
-                                src={jsImg}
-                                alt="JavaScript icon"
-                            />
-                        </a>
-                        <p>JavaScript</p>
-                    </div>
-                    <div className={styles.item}>
-                        <a
-                            href=" https://www.typescriptlang.org/docs/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <img
-                                className={styles.typescript}
-                                src={tsImg}
-                                alt="TypeScript black icon"
-                            />
-                        </a>
-                        <p>TypeScript</p>
-                    </div>
-                    <div className={styles.item}>
-                        <a
-                            href="https://tailwindcss.com/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <img
-                                className={styles.tailwind}
-                                src={tailwindImg}
-                                alt="Tailwind icon"
-                            />
-                        </a>
-                        <p>Tailwind</p>
-                    </div>
-                    <div className={styles.item}>
-                        <a
-                            href="https://sass-lang.com/"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <img
-                                className={styles.sass}
-                                src={sassImg}
-                                alt="SASS icon"
-                            />
-                        </a>
-                        <p>SASS</p>
-                    </div>
-                    <div className={styles.item}>
-                        <a
-                            href="https://git-scm.com/doc"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <img
-                                className={styles.git}
-                                src={gitImg}
-                                alt="GIT icon"
-                            />
-                        </a>
-                        <p>GIT</p>
-                    </div>
-                    <div className={styles.item}>
-                        <a
-                            href="https://developer.mozilla.org/es/docs/Web/HTML"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <img
-                                className={styles.html}
-                                src={htmlImg}
-                                alt="HTML icon"
-                            />
-                        </a>
-                        <p>HTML</p>
-                    </div>
-                    <div className={styles.item}>
-                        <a
-                            href="https://developer.mozilla.org/es/docs/Web/CSS"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <img
-                                className={styles.css}
-                                src={cssImg}
-                                alt="CSS icon"
-                            />
-                        </a>
-                        <p>CSS</p>
-                    </div>
-                </Zoom>
-            </article>
-        </section>
-    );
+	return (
+		<section className={styles.section} id='tools'>
+			<h3>Tecnologías</h3>
+			<article className={styles.article}>
+				<Zoom cascade duration={200} triggerOnce>
+					<div className={styles.item}>
+						<a href='https://react.dev/' target='_blank' rel='noreferrer'>
+							<img className={styles.react} src={reactImg} alt='React icon' />
+						</a>
+						<p>React</p>
+					</div>
+					<div className={styles.item}>
+						<a href='https://astro.build/' target='_blank' rel='noreferrer'>
+							<img className={styles.git} src={astroImg} alt='Astro icon' />
+						</a>
+						<p>Astro</p>
+					</div>
+					<div className={styles.item}>
+						<a href='https://vitejs.dev/' target='_blank' rel='noreferrer'>
+							<img className={styles.vite} src={viteImg} alt='Vite icon' />
+						</a>
+						<p>Vite</p>
+					</div>
+					<div className={styles.item}>
+						<a href='https://nextjs.org/' target='_blank' rel='noreferrer'>
+							<img
+								className={styles.nextjs}
+								src={nextjsImg}
+								alt='Next JS icon'
+							/>
+						</a>
+						<p>Next JS</p>
+					</div>
+					<div className={styles.item}>
+						<a
+							href='https://developer.mozilla.org/es/docs/Web/JavaScript'
+							target='_blank'
+							rel='noreferrer'
+						>
+							<img
+								className={styles.javascript}
+								src={jsImg}
+								alt='JavaScript icon'
+							/>
+						</a>
+						<p>JavaScript</p>
+					</div>
+					<div className={styles.item}>
+						<a
+							href=' https://www.typescriptlang.org/docs/'
+							target='_blank'
+							rel='noreferrer'
+						>
+							<img
+								className={styles.typescript}
+								src={tsImg}
+								alt='TypeScript black icon'
+							/>
+						</a>
+						<p>TypeScript</p>
+					</div>
+					<div className={styles.item}>
+						<a href='https://tailwindcss.com/' target='_blank' rel='noreferrer'>
+							<img
+								className={styles.tailwind}
+								src={tailwindImg}
+								alt='Tailwind icon'
+							/>
+						</a>
+						<p>Tailwind</p>
+					</div>
+					<div className={styles.item}>
+						<a href='https://sass-lang.com/' target='_blank' rel='noreferrer'>
+							<img className={styles.sass} src={sassImg} alt='SASS icon' />
+						</a>
+						<p>SASS</p>
+					</div>
+					<div className={styles.item}>
+						<a href='https://git-scm.com/doc' target='_blank' rel='noreferrer'>
+							<img className={styles.git} src={gitImg} alt='GIT icon' />
+						</a>
+						<p>GIT</p>
+					</div>
+					<div className={styles.item}>
+						<a
+							href='https://developer.mozilla.org/es/docs/Web/HTML'
+							target='_blank'
+							rel='noreferrer'
+						>
+							<img className={styles.html} src={htmlImg} alt='HTML icon' />
+						</a>
+						<p>HTML</p>
+					</div>
+					<div className={styles.item}>
+						<a
+							href='https://developer.mozilla.org/es/docs/Web/CSS'
+							target='_blank'
+							rel='noreferrer'
+						>
+							<img className={styles.css} src={cssImg} alt='CSS icon' />
+						</a>
+						<p>CSS</p>
+					</div>
+				</Zoom>
+			</article>
+		</section>
+	);
 };
 export default Tools;
