01a01575ac90f07f9076bd2238d2c904562275e6
fedeminatta <minattafederico@gmail.com>
2024-07-28 15:45:06 -0300

    fix: scroll margin top

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,47 +8,43 @@ import { Bounce } from 'react-awesome-reveal';
 import Iconos from './Iconos';
 
 const Projects = () => {
-    const [showMore, setShowMore] = useState(false);
+	const [showMore, setShowMore] = useState(false);
 
-    return (
-        <div className={styles.container}>
-            <main
-                className={styles.main}
-                id="Main"
-                style={{
-                    maxHeight: `${showMore == true ? 'none' : '980px'}`,
-                }}
-            >
-                <h2>Proyectos</h2>
-                <section>
-                    <Bounce cascade damping={0.04} duration={800} triggerOnce>
-                        {Proyectos.map((proyecto) => (
-                            <Project
-                                key={proyecto.id}
-                                titulo={proyecto.titulo}
-                                descripcion={proyecto.descripcion}
-                                img={proyecto.img}
-                                url={proyecto.url}
-                            >
-                                {proyecto.descripcion.map((desc) => (
-                                    <Iconos
-                                        title={desc.toLowerCase()}
-                                        key={desc}
-                                    />
-                                ))}
-                            </Project>
-                        ))}
-                    </Bounce>
-                </section>
-            </main>
+	return (
+		<div id='Main' className={styles.container}>
+			<main
+				className={styles.main}
+				style={{
+					maxHeight: `${showMore == true ? 'none' : '980px'}`,
+				}}
+			>
+				<h2>Proyectos</h2>
+				<section>
+					<Bounce cascade damping={0.04} duration={800} triggerOnce>
+						{Proyectos.map((proyecto) => (
+							<Project
+								key={proyecto.id}
+								titulo={proyecto.titulo}
+								descripcion={proyecto.descripcion}
+								img={proyecto.img}
+								url={proyecto.url}
+							>
+								{proyecto.descripcion.map((desc) => (
+									<Iconos title={desc.toLowerCase()} key={desc} />
+								))}
+							</Project>
+						))}
+					</Bounce>
+				</section>
+			</main>
 
-            <ShowMoreBtn
-                text={showMore == false ? 'Mostrar más' : 'Mostrar menos'}
-                setShowMore={setShowMore}
-                showMore={showMore}
-            />
-        </div>
-    );
+			<ShowMoreBtn
+				text={showMore == false ? 'Mostrar más' : 'Mostrar menos'}
+				setShowMore={setShowMore}
+				showMore={showMore}
+			/>
+		</div>
+	);
 };
 
 export default Projects;
